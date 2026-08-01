b65b974278f12277361e7aea94b8886ea3dfd5c9
Divyanshi Rajput <divyanshirajput70@gmail.com>
2026-08-01 21:36:39 +0530

    Time: 1 ms (98.87%) | Memory: 44.6 MB (80.14%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        // long first = Long.MIN_VALUE;
-        // long second = Long.MIN_VALUE;
-        // long third = Long.MIN_VALUE;
+         long first = Long.MIN_VALUE;
+         long second = Long.MIN_VALUE;
+         long third = Long.MIN_VALUE;
         // for(int i = 0; i<nums.length; i++){
         //      int num = nums[i];
         //     if(num == first || num == second || num == third){
@@ -26,24 +26,46 @@ class Solution {
         //     return (int)first;
         // }
         // return (int)third;
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        int count = 1;
-        int prev = nums[nums.length - 1];
+        // int count = 1;
+        // int prev = nums[nums.length - 1];
 
-        for(int i = nums.length - 2; i >= 0; i--){
+        // for(int i = nums.length - 2; i >= 0; i--){
 
-            if(nums[i] != prev){
-                count++;
-                prev = nums[i];
+        //     if(nums[i] != prev){
+        //         count++;
+        //         prev = nums[i];
+        //     }
+
+        //     if(count == 3){
+        //         return nums[i];
+        //     }
+        // }
+
+        // return nums[nums.length - 1];
+        for(int i = 0; i < nums.length;i++){
+            int num = nums[i];
+            if(num == first || num == second || num == third){
+                continue;
             }
-
-            if(count == 3){
-                return nums[i];
+            if(num > first){
+                third = second;
+                second = first;
+                first = num;
+            }
+            else if(num > second){
+                third = second;
+                second = num;
+            }
+            else if(num > third){
+                third = num;
             }
         }
-
-        return nums[nums.length - 1];
+        if(third == Long.MIN_VALUE){
+            return (int)first;
+        }
+        return (int)third;
         
     }
 }
